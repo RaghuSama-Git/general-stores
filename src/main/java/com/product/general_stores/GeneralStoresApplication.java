4afcc5d2fd1ee00ffbf5e23c4402e8c6e42e3365
RaghuSama-Git <raghusama99@gmail.com>
2025-09-02 16:32:16 +0530

    hey its first commit

---
+++ src/main/java/com/product/general_stores/GeneralStoresApplication.java
@@ -0,0 +1,13 @@
+package com.product.general_stores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralStoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralStoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/product/general_stores/GeneralStoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.general_stores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralStoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
